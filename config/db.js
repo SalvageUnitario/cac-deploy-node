45cbde91ac2e3c7a7d00cea6a10f784923d6816a
SalvageUnitario <57801134+SalvageUnitario@users.noreply.github.com>
2024-07-07 14:03:28 -0300

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -2,19 +2,19 @@ import { createPool } from 'mysql2/promise';
 
 // Configurar el pool de conexiones
 const pool = createPool({
-//   host: process.env.MYSQL_ADDON_HOST,
-//   user: process.env.MYSQL_ADDON_USER,
-//   password: process.env.MYSQL_ADDON_PASSWORD,
-//   database: process.env.MYSQL_ADDON_DB,
-//   connectionLimit: 10,
-//   connectTimeout: 10000
-
-  host: '192.168.200.20',
-  user: 'mariano',
-  password: 'Telecom1234@',
-  database: 'libreria',
-  connectionLimit: 5,
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  connectionLimit: 10,
   connectTimeout: 10000
+
+//   host: '192.168.200.20',
+//   user: 'mariano',
+//   password: 'Telecom1234@',
+//   database: 'libreria',
+//   connectionLimit: 5,
+//   connectTimeout: 10000
 
 });
 
